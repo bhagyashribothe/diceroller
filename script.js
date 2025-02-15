0531964aee6f53af61b4dc6b003b4c94ee6a9763
bhagyashribothe <bhagyashribothe14@gmail.com>
2025-02-15 21:40:56 +0530

    java script file added

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-function rollDice() {
+function rollDice() 
+{
     const diceImages = document.querySelectorAll('.dice');
     const randomNumber = Math.floor(Math.random() * 6) + 1;
     const rolledNumberElement = document.getElementById('rolledNumber');
 
-    // Update the rolled number display
+    
     rolledNumberElement.innerText = randomNumber;
 
-    // Update the dice image based on the rolled number
+    
     diceImages.forEach(dice => {
-        if (parseInt(dice.dataset.value) === randomNumber) {
-            dice.style.border = '5px solid red';  // Highlight the dice
-        } else {
+        if (parseInt(dice.dataset.value) === randomNumber) 
+            {
+            dice.style.border = '5px solid red';  
+        } 
+        else 
+        {
             dice.style.border = 'none';
         }
     });
